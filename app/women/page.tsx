f817b2ccab91f63afe9a423f0881d68a88a94234
Ajay Sharma <sharmaajan49.95@gmail.com>
2026-04-16 19:37:17 +0530

    eleven commit

--- app/women/page.tsx
+++ app/women/page.tsx
@@ -38,8 +38,8 @@ export default function WomenPage() {
 
   // Women’s luxury brands
   const womenBrands = [
-    'Gucci', 'Prada', 'Chanel', 'Louis Vuitton',
-    'Jimmy Choo', 'Manolo Blahnik', 'Christian Louboutin', 'Tory Burch'
+    'Campus', 'Skechers', 'Reebok', 'Bata',
+    'Nike',
   ];
 
   return (
@@ -66,7 +66,7 @@ export default function WomenPage() {
             <div className="flex justify-center mb-6">
               <Sparkles className="text-[#d4af37] animate-bounce" size={24} />
             </div>
-            <h1 className="text-6xl md:text-8xl lg:text-[7rem] font-black italic tracking-tighter leading-[0.8] mb-6">
+            <h1 className="text-6xl md:text-8xl lg:text-[5rem] font-black italic tracking-tighter leading-[0.8] mb-6">
               GRACE <br />
               <span className="font-light text-transparent bg-clip-text bg-gradient-to-r from-[#d4af37] via-[#fff] to-[#d4af37]">
                 IN EVERY STEP.
